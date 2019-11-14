6055ecde8445cc03db336a234df45edb2716a2a7
Alex Wallis <aldwallis@gmail.com>
2019-11-14 15:25:59 -0800

    AirBnB style modifactions for Reviews.jsx

--- client/components/Reviews.jsx
+++ client/components/Reviews.jsx
@@ -1,56 +1,86 @@
-import styled, {css} from 'styled-components';
-import StarRatings from './StarRating.jsx';
-import { useState } from 'react';
+/* eslint-disable react/jsx-curly-brace-presence */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable no-shadow */
+/* eslint-disable react/jsx-boolean-value */
+/* eslint-disable no-script-url */
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import StarRatings from './StarRating';
 
-const ReviewDescription = ({desc}) => {
+const ReviewDescription = ({ desc }) => {
   const [moreContent, setMoreContent] = useState(false);
   const [showReadMore, setShowReadMore] = useState(true);
-  return(
+  return (
     <Description>
-      <span>{desc.slice(0,800)}</span>
-      {(desc.length > 800) && 
-        <><StyledDescription as='a' href="javascript:;" isLink={true} setVisible={showReadMore} onClick={() => {
-            setMoreContent(true);
-            setShowReadMore(false);
-          }}>Read full review</StyledDescription>
-          <StyledDescription setVisible={moreContent}>{desc.slice(80)}</StyledDescription></>}
+      <span>{desc.slice(0, 800)}</span>
+      {(desc.length > 800)
+      && (
+        <>
+          <StyledDescription
+            as="a"
+            href="javascript:;"
+            isLink={true}
+            setVisible={showReadMore}
+            onClick={() => {
+              setMoreContent(true);
+              setShowReadMore(false);
+            }}
+          >
+            Read full review
+          </StyledDescription>
+          <StyledDescription
+            setVisible={moreContent}
+          >
+            {desc.slice(80)}
+          </StyledDescription>
+        </>
+      )}
     </Description>
   );
 };
 
-const Reviews = ({reviewDetails, productCondition, pagingAndSorting, noOfRatings}) => {
-  const month_names =["Jan","Feb","Mar",
-  "Apr","May","Jun",
-  "Jul","Aug","Sep",
-  "Oct","Nov","Dec"];
-  if(reviewDetails.length > 0) {
-    return <>{reviewDetails.map((rec) => {
-      const created_on = new Date(rec.created_on);
-      const date = `${month_names[created_on.getMonth()]} ${created_on.getDate()}, ${created_on.getFullYear()}`;
-      return(
-        <>
-        <Wrapper key={rec.id}>
-          <ReviewSection>
-            <StarRatings avgRatings={rec.ratings} starName={'star'+rec.id} starSize={'small'}></StarRatings>
-            <div>by <StyledDescription isLink={true}>{rec.user.name}</StyledDescription></div>
-            <ReviewDate>{date}</ReviewDate>
-          </ReviewSection>
-          <ReviewContent>
-            <Title>{rec.title}</Title>
-            <ReviewDescription desc={rec.description}></ReviewDescription>
-            <div>
-              <ReviewAttr>Verified purchase: </ReviewAttr>
-              <ReviewValue>{rec.report_abuse.toString()}</ReviewValue>
-              <ReviewAttr>Condition: </ReviewAttr>
-              <ReviewValue>{productCondition}</ReviewValue>
-            </div>
-          </ReviewContent>
-        </Wrapper> 
-        </>); 
-    })}</>
-  } else {
-    return null;
+const Reviews = ({
+  // eslint-disable-next-line no-unused-vars
+  reviewDetails, productCondition, pagingAndSorting, noOfRatings,
+}) => {
+  // changed month_names to monthNames
+  const monthNames = ['Jan', 'Feb', 'Mar',
+    'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep',
+    'Oct', 'Nov', 'Dec'];
+  if (reviewDetails.length > 0) {
+    return (
+      <>
+        {reviewDetails.map((rec) => {
+          const createdOn = new Date(rec.createdOn);
+          const date = `${monthNames[createdOn.getMonth()]} ${createdOn.getDate()}, ${createdOn.getFullYear()}`;
+          return (
+            <>
+              <Wrapper key={rec.id}>
+                <ReviewSection>
+                  <StarRatings avgRatings={rec.ratings} starName={`star${rec.id}`} starSize={'small'} />
+                  <div>by <StyledDescription isLink={true}>{rec.user.name}</StyledDescription></div>
+                  <ReviewDate>{date}</ReviewDate>
+                </ReviewSection>
+                <ReviewContent>
+                  <Title>{rec.title}</Title>
+                  <ReviewDescription desc={rec.description} />
+                  <div>
+                    <ReviewAttr>Verified purchase: </ReviewAttr>
+                    <ReviewValue>{rec.report_abuse.toString()}</ReviewValue>
+                    <ReviewAttr>Condition: </ReviewAttr>
+                    <ReviewValue>{productCondition}</ReviewValue>
+                  </div>
+                </ReviewContent>
+              </Wrapper>
+            </>
+          );
+        })}
+      </>
+    );
   }
+  return null;
 };
 
 export default Reviews;
@@ -58,7 +88,7 @@ export default Reviews;
 
 const Wrapper = styled.div`
   display: flex;
-  font-size: ${props => props.theme.fontSize};
+  font-size: ${(props) => props.theme.fontSize};
   margin-top: 30px;
   @media screen and (max-width: 790px) {
     display: block;
@@ -67,14 +97,14 @@ const Wrapper = styled.div`
 
 const ReviewDate = styled.div`
   color: #999;
-  font-size: ${props => props.theme.fontSize};
+  font-size: ${(props) => props.theme.fontSize};
 `;
 
 const ReviewSection = styled.div`
   width:25%;
   div {
     margin-top: 10px;
-  } 
+  }
   @media screen and (max-width: 790px) {
     width: 100%;
   }
@@ -113,11 +143,10 @@ const ReviewContent = styled.div`
 `;
 
 const StyledDescription = styled.span`
-  display: ${props => (props.setVisible === false)?'none':'inline'};
-  ${props => props.isLink &&
-    css`
-    color: ${props => props.theme.linkColor};
-    margin-left:5px; 
+  display: ${(props) => (((props).setVisible === false) ? 'none' : 'inline')};
+  ${(props) => props.isLink
+    && css`
+    color: ${(props) => props.theme.linkColor};
+    margin-left:5px;
   `};
 `;
-
