8f97401a8b18db89f42afe416f503bec4762c7b5
Alex Wallis <aldwallis@gmail.com>
2019-11-18 13:46:59 -0800

    Complete creation of endpoints

--- server/controllers.js
+++ server/controllers.js
@@ -20,6 +20,52 @@ module.exports = {
       .catch((err) => { console.log(err); callback(err); });
   },
 
+  getRatings: () => {
+    return new Promise((resolve, reject) => {
+      db.Review.findAll({
+        attributes: ['id', 'ratings', 'productId', 'isProductProp1Good', 'isProductProp2Good', 'isProductProp3Good'],
+      })
+      .then(all => {
+        //console.log('db.Review.findAll: ',all);
+        resolve(all);
+      })
+      .catch(err => {
+        //console.error('db.Review.findAll: ',err);
+        reject(err);
+      });
+    })
+  },
+
+  getReviews1: () => {
+    return new Promise((resolve, reject) => {
+      db.Review.findAll({
+        attributes: ['id', 'ratings', 'title', 'description', 'report_abuse', 'created_on', 'productId'],
+        offset: 5,
+        //Limit works but currently the max w/ this innerjoin is 5.
+        limit: 5,
+        //I believe include works like an inner join? and here the primary key is id
+        include: [{
+          model: db.ReviewFeedback,
+          attributes: ['isHelpful'],
+          where: { reviewId: db.Sequelize.col('reviews.id') }
+        },
+        {
+          model: db.User,
+          attributes: ['name'],
+          where: { userId: db.Sequelize.col('user.id') }
+        }]
+      })
+      .then(result => {
+        console.log('getReviews1: ',result);
+        resolve(result);
+      })
+      .catch(error => {
+        console.log('getReviews error: ',error);
+        reject(error);
+      })
+    })
+  },
+
   getReviews: (productId, pagingAndSorting, callback) => {
     const p1 = db.Review.findAll({
       offset: pagingAndSorting.offset,
@@ -28,13 +74,8 @@ module.exports = {
       include: [{
         model: db.ReviewFeedback,
         attributes: ['isHelpful'],
-        where: { reviewId: db.Sequelize.col('reviews.id') } 
-      }, 
-      // {
-      //   model: db.ReviewImage,
-      //   attributes: ['imageUrl'],
-      //   where: { reviewId: db.Sequelize.col('reviews.id') }
-      // }, 
+        where: { reviewId: db.Sequelize.col('reviews.id') }
+      },
       {
         model: db.User,
         attributes: ['name'],
@@ -53,6 +94,6 @@ module.exports = {
     });
     Promise.all([p1, p2])
       .then((res) => { callback(null, { reviews: res[0], productDetails: res[1] }); })
-      .catch((err) => { console.log(err); callback(err); });
+      .catch((err) => { console.log('controller.js Promise: ',err); callback(err); });
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 
 const PORT = process.env.PORT || 3004;
-const app = require('./app.js');
+//const app = require('./app.js');
+const app = require('./app1.js');
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
